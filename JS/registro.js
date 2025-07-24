2cae17d0a36b27f7e5157eacbe002e9c6f7515d7
jordizavaleta <jrddnl@gmail.com>
2025-07-23 22:48:54 -0500

    Add files via upload

--- JS/registro.js
+++ JS/registro.js
@@ -8,5 +8,5 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   sessionStorage.setItem('nombreUsuario', username); // Guarda el nombre
 
   // Redirigir a la página principal
-  window.location.href = '../BullyingCero.html'; // o ajusta según tu estructura real
+  window.location.href = '../index.html'; // o ajusta según tu estructura real
 });
